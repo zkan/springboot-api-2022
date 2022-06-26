1d124b7432d9d66b8ae33f168a6340a516d2c716
zkan <zkan.cs@gmail.com>
2022-06-26 13:46:42 +0700

    Initialize database with some data

--- src/main/java/com/example/springbootapi2022/SpringbootApi2022Application.java
+++ src/main/java/com/example/springbootapi2022/SpringbootApi2022Application.java
@@ -2,9 +2,13 @@ package com.example.springbootapi2022;
 
 import com.example.springbootapi2022.dependency.Address;
 import com.example.springbootapi2022.dependency.Customer;
+import com.example.springbootapi2022.entity.Employee;
+import com.example.springbootapi2022.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootApi2022Application {
@@ -18,6 +22,28 @@ public class SpringbootApi2022Application {
 //		Address address = context.getBean(Address.class);
 		System.out.println("address no: " + customer.getAddress().getAddressNo());
 		System.out.println("address name: " + customer.getAddress().getAddressName());
+	}
+
+	// What we return here becomes Spring Bean
+	@Bean
+	public CommandLineRunner initialize(EmployeeRepository employeeRepository) {
+		return (args) -> {
+			{
+				Employee employee = new Employee();
+				employee.setFirstName("Kan");
+				employee.setLastName("Ouivirach");
+
+				employeeRepository.save(employee);
+			}
+
+			{
+				Employee employee = new Employee();
+				employee.setFirstName("John");
+				employee.setLastName("Doe");
+
+				employeeRepository.save(employee);
+			}
+		};
 	}
 
 }
