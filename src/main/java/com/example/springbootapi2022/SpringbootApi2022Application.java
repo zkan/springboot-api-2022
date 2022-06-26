dd060c62139826d1804949f8453b2ef9549d2447
zkan <zkan.cs@gmail.com>
2022-06-26 10:21:15 +0700

    Initialize Spring Boot project :bear:

---
+++ src/main/java/com/example/springbootapi2022/SpringbootApi2022Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootapi2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApi2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApi2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootapi2022/SpringbootApi2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootapi2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApi2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
