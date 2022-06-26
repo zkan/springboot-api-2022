c757c5d07060c1be179cce9e2053b084c73e7a4a
zkan <zkan.cs@gmail.com>
2022-06-26 11:01:34 +0700

    Get employee by id

--- src/main/java/com/example/springbootapi2022/controller/EmployeeController.java
+++ src/main/java/com/example/springbootapi2022/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.springbootapi2022.controller;
 
 import com.example.springbootapi2022.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -18,6 +19,18 @@ public class EmployeeController {
         employee.setLastName("Ouivirach");
 
         return Arrays.asList(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable Integer id) {
+        System.out.println("id: " + id);
+
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setFirstName("Kan");
+        employee.setLastName("Ouivirach");
+
+        return employee;
     }
 
 }
